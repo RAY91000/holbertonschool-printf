ae8fd86634162a75117030c7e11fd943b840ffdb
root <ray.dotropg@mail.com>
2024-11-25 20:05:08 +0000

    debut de _printf.c (avec seulement %)

--- _printf.c
+++ _printf.c
@@ -7,6 +7,22 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i;
-	int j;
+	int counter = 0;
 
-	if 
+	if (format == NULL || format[0] == '\0' ||
+			(format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++) {
+		if (format[i] != '%') {
+			counter += _putchar('%');
+			counter += _putchar(format[i]);
+	}
+	else {
+		counter += _putchar(format[i]);
+	}
+	}
+va_end(args);
+return counter;
+}
