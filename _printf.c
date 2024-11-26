8d0ec60ca1f5a38e361992989884ee25528c252e
Hikarikedo <keriandouidi01@gmail.com>
2024-11-26 15:17:25 +0100

    reteived -printf.c

--- _printf.c
+++ _printf.c
@@ -1,12 +1,21 @@
 #include "main.h"
-/*
- *
- *
+/*_printf - Basically a copy of the printf function
+ *@format: character string
+ *Return: 
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i;
-	int j;
+	int count = 0;
 
-	if 
+	if (format == NULL || format[0] == '\0' ||
+	(format [0] = '%' && format[1] = '\0'))
+	       return (-1);
+	va_start(args, format);
+
+	for (i = 0, format[i] != '\0', i++)
+	{
+		if (format[i] = '%')
+		{
+				
