198fa4e56452b1944fdfbd00f86bcd4d734e3d90
Hikarikedo <keriandouidi01@gmail.com>
2024-11-26 16:47:19 +0100

    added percent.c

---
+++ percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_printf_percent - prints the % symbol 
+ *@args: function argument
+ *Return: 0 = Success
+ */
+int _printf_percent(va_list args)
+{
+	void(args)
+	_putchar('%');
+	return (1);
+}
