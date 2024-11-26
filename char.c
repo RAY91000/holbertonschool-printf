3572f4b824fc71f39c08e40be1a31611cbf4db59
Hikarikedo <keriandouidi01@gmail.com>
2024-11-26 16:26:43 +0100

    changed the return

--- char.c
+++ char.c
@@ -2,10 +2,10 @@
 /**
  *_printf_char - function that prints a single character
  *@args: function argument
- *Return: 0 = Success
+ *Return: 1 = one character was printed
  */
 int _printf_char(va_list args)
 {
 	_putchar(va_arg(args, int));
-	return (0);
+	return (1);
 }
