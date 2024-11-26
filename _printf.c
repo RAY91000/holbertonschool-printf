5e79c98a9600e86347d33b55a09179b0b7543671
Hikarikedo <keriandouidi01@gmail.com>
2024-11-26 15:19:29 +0100

    retreived _printf.c

--- _printf.c
+++ _printf.c
@@ -1,21 +1,28 @@
 #include "main.h"
-/*_printf - Basically a copy of the printf function
- *@format: character string
- *Return: 
+/*
+ *
+ *
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i;
-	int count = 0;
+	int counter = 0;
 
 	if (format == NULL || format[0] == '\0' ||
-	(format [0] = '%' && format[1] = '\0'))
-	       return (-1);
+			(format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	
 	va_start(args, format);
-
-	for (i = 0, format[i] != '\0', i++)
-	{
-		if (format[i] = '%')
-		{
-				
+	for (i = 0; format[i] != '\0'; i++) {
+		if (format[i] != '%') {
+			counter += _putchar('%');
+			counter += _putchar(format[i]);
+	}
+	else {
+		counter += _putchar(format[i]);
+	}
+	}
+va_end(args);
+return counter;
+}		
