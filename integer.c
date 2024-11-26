a7c3153171e4e9e00ecfba81f5a8184d3a2be422
root <ray.dotropg@gmail.com>
2024-11-26 16:20:03 +0000

    code of file integer.c

---
+++ integer.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/*
+ * printf_int - prints integer
+ * @ap: contain the integer
+ *
+ * Return: the number of character printed.
+ */
+
+int printf_int(va_list ap)
+{
+	int n = va_arg(ap, int);
+	unsigned int num;
+	int len = 0;
+	int div = 1;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	while (num / div > 9)
+		div *= 	10;
+	while (div != 0)
+	{
+		len += _putchar((num / div) + '\0');
+		div /= 10;
+	}
+	return (len);
+}
