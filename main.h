25c5df486aaea6880f376f7a700a3b2d39d11017
Hikarikedo <keriandouidi01@gmail.com>
2024-11-27 16:50:28 +0100

    readding everything

--- main.h
+++ main.h
@@ -10,4 +10,11 @@ int printf_string(va_list args);
 int _printf_percent(va_list args);
 int printf_int(va_list ap);
 int _printf_deci(va_list ap);
+
+typedef struct format_specifier
+{
+	char specifier;
+	int (*func)(va_list);
+}
+format_specifier_t;
 #endif
