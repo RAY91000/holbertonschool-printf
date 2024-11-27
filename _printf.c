d0f3e54f87ff041a42c7ab9e85ebfae5ceaa8a55
root <ray.dotropg@gmail.com>
2024-11-27 11:10:25 +0000

    correction du printf non complete

--- _printf.c
+++ _printf.c
@@ -1,28 +1,59 @@
 #include "main.h"
+#include <stdarg.h>
+
 /**
  *_printf - 
  *@format:
  *return:
  */
+
+typedef struct format_specifier {
+	char specifier;
+	int (*func)(va_list);
+}
+
+format_specifier_t;
+
+format_specifier_t specifiers[] = {
+	{'i', _printf_char},
+	{'s', printf_string},
+	{'d',_printf_deci},
+	{'i', printf_int},
+	{'%', _printf_percent},
+	{'\0', NULL},
+};
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i;
+	int i = 0; int j;
 	int counter = 0;
 
-	if (format == NULL || format[0] == '\0' ||
-			(format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || format[0] == '\0')
+	{
 		return (-1);
-	
+	}
+
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++) {
+	while (format[i] != '\0') {
 		if (format[i] != '%') {
-			counter += _putchar('%');
 			counter += _putchar(format[i]);
-	}
+		}
 	else {
-		counter += _putchar(format[i]);
+		for (j = 0; specifiers[j].specifier != '\0'; j++) {
+			if (format[i + 1] == specifiers[j].specifier) {
+				counter += specifiers[j].func(args);
+				i++;
+				break;
 	}
+	}
+		if (specifiers[j].specifier == '\0') {
+			counter += _putchar(format[i]);
+			counter += _putchar(format[i + 1]);
+			i++;
+		}
+	}
+	i++;
 	}
 va_end(args);
 return counter;
