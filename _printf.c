499f41435a7985bd9048fdf879bec040fa4e2144
root <ray.dtropg@gmail.com>
2024-11-27 16:39:50 +0000

    reduging _printf.c

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 
 	i = 0, counter = 0;
 
-	if (format == NULL || format[0] == '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(args, format);
